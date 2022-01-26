b17a7f6e4cdedebb1c0dfe542dbac732c51f51a1
ANTONIO ZILLI ZANOLLI <antonio.zanolli@pmc.local>
2022-01-26 11:38:22 -0300

    First

---
+++ BulkyBookWeb/app.cs
@@ -0,0 +1,42 @@
+﻿internal class app
+{
+    internal static void UseHttpsRedirection()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UseStaticFiles()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UseRouting()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UseAuthorization()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UseExceptionHandler(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void MapControllerRoute(string name, string pattern)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void UseHsts()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void Run()
+    {
+        throw new NotImplementedException();
+    }
+}
